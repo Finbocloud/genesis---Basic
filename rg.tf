2475d9224d2f5959f2567b40c92fc8c2df8ec2cd
samuelazure <durhamstar1@gmail.com>
2024-08-08 17:47:20 +0100

    genesis locaation

--- rg.tf
+++ rg.tf
@@ -1,4 +1,4 @@
 resource "azurerm_resource_group" "this_rg" {
-  name     = "genesis-rg"
-  location = "uk south"
+  name     = var.rg-name
+  location = var.rg-location
 }
---
+++ var.tf
@@ -0,0 +1,13 @@
+variable "rg-name" {
+    type = string
+    default = "rg-genesis"
+    description = "resource group name"
+  
+}
+
+variable "rg-location"{
+    type = string
+    default ="uk south"
+    description = "resource group location"
+    
+}
