811ca7d93bf3e0bfb0732ec1bdd385e7e736a8b4
mr.raerae@outlook.com <mrraymondchilaka>
2024-08-12 20:29:18 +0100

    azure resources

---
+++ db.tf
@@ -0,0 +1,14 @@
+
+resource "azurerm_mysql_flexible_server" "this_sql_fs" {
+  name                   = "ray-fs"
+  resource_group_name    = azurerm_resource_group.this_rg.name
+  location               = azurerm_resource_group.this_rg.location
+  administrator_login    = "psqladmin"
+  administrator_password = "H@Sh1CoR3!"
+  backup_retention_days  = 7
+  delegated_subnet_id    = azurerm_subnet.this_subnet.id
+  private_dns_zone_id    = azurerm_private_dns_zone.this_dns.id
+  sku_name               = "GP_Standard_D2ds_v4"
+
+  depends_on = [azurerm_private_dns_zone_virtual_network_link.example]
+}
--- var.tf
+++ var.tf
@@ -11,3 +11,20 @@ variable "rg-location"{
     description = "resource group location"
     
 }
+variable "vnet-name" {
+  type = string
+  default = "vnet-genesis"
+  description = "genesis vnet"
+}
+
+variable "subnet-name"{
+    type = string
+    default = "subnet-genesis"
+    description = "gensis subnet"
+}
+variable "this_sql_fs-name" {
+  type = string
+  default = "this_sql_fs-geneis"
+  description = "genesis fs"
+
+}
