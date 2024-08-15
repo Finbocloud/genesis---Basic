5a3f46aed6a7d235bc954e85cbb4d249eac4c16c
mr.raerae@outlook.com <mrraymondchilaka>
2024-08-15 17:02:59 +0100

    genesis nsg

--- networking.tf
+++ networking.tf
@@ -13,8 +13,3 @@ resource "azurerm_subnet" "this_subnet" {
   address_prefixes     = ["10.0.2.0/24"]
   }
 
-resource "azurerm_network_security_group" "this_nsg" {
-  name                = "ray-nsg"
-  location            = azurerm_resource_group.this_rg.location
-  resource_group_name = azurerm_resource_group.this_rg.name
-  }
